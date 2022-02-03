fc6563a67c145529e9587ad78ed33805b9282fa1
Fastroof <lytvdan@gmail.com>
2022-02-03 08:45:31 +0200

    Delete unused imports

--- src/main/java/ua/com/apricortka/apricotkacash/controller/MainController.java
+++ src/main/java/ua/com/apricortka/apricotkacash/controller/MainController.java
@@ -5,11 +5,8 @@ import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
--- src/main/java/ua/com/apricortka/apricotkacash/controller/MainRestController.java
+++ src/main/java/ua/com/apricortka/apricotkacash/controller/MainRestController.java
@@ -7,7 +7,6 @@ import org.json.JSONTokener;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import ua.com.apricortka.apricotkacash.enums.TimeInterval;
